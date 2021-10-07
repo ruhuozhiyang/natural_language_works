92d1a826d7269d2b8ea61446d8d7f6bcd918bbd6
Ruyangchen <3226717170@qq.com>
2021-10-07 17:06:28 +0800

    cnn update

--- image_classification/CNN/cnn_train.py
+++ image_classification/CNN/cnn_train.py
@@ -1,11 +1,25 @@
+from keras import optimizers
 from keras.models import Sequential
 from keras.layers import Conv2D, MaxPooling2D
-from keras.layers import Activation, Dropout, Flatten, Dense
+from keras.layers import Dropout, Flatten, Dense
 from keras.preprocessing.image import ImageDataGenerator
 import matplotlib
+
 matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 from keras.utils.vis_utils import plot_model
+import keras.backend as K
+from keras.callbacks import LearningRateScheduler
+
+
+def scheduler(epoch):
+    if epoch % 10 == 0 and epoch != 0:
+        lr = K.get_value(model.optimizer.lr)
+        K.set_value(model.optimizer.lr, lr * 0.1)
+    return K.get_value(model.optimizer.lr)
+
+
+reduce_lr = LearningRateScheduler(scheduler)
 
 # 全局常量
 train_data_dir = '../data/train'
@@ -15,7 +29,7 @@ result_dir = './result'
 
 train_iteration_count = 100
 val_iteration_count = 25
-epochs = 10
+epochs = 50
 batch_size = 20
 img_width, img_height = 128, 128
 input_shape = (img_width, img_height, 3)
@@ -23,23 +37,27 @@ input_shape = (img_width, img_height, 3)
 # 模型的具体内容
 model = Sequential()
 # 卷积层作为模型第一层时候，必须提供input_shape参数.
-model.add(Conv2D(16, (2, 2), padding="same", activation='relu', input_shape=input_shape))
+model.add(Conv2D(32, (2, 2), padding="same", activation='relu', input_shape=input_shape))
 model.add(MaxPooling2D(pool_size=2))
 
 # Dropout应用于输入.包括在训练中每次更新时, 将输入单元的按比率随机设置为 0, 这有助于防止过拟合.
-model.add(Dropout(0.25))
-model.add(Conv2D(32, (3, 3), padding="same", activation='relu'))
+model.add(Conv2D(64, (2, 2), padding="same", activation='relu'))
 model.add(MaxPooling2D(pool_size=2))
 
-model.add(Dropout(0.25))
-model.add(Conv2D(64, (3, 3), padding="same", activation='relu'))
-model.add(MaxPooling2D(pool_size=2))
+# model.add(Conv2D(64, (2, 2), padding="same", activation='relu'))
+# model.add(MaxPooling2D(pool_size=2))
+#
+# model.add(Conv2D(128, (2, 2), padding="same", activation='relu'))
+# model.add(MaxPooling2D(pool_size=2))
 
 model.add(Flatten())  # 扁平层
-model.add(Dense(500, activation='relu'))  # 全连接
+model.add(Dropout(0.5))
+model.add(Dense(1024, activation='relu'))  # 全连接
 model.add(Dense(2, activation='softmax'))
+
 # compile用于配置训练模型: adam的默认学习率为0.001、loss配置损失函数、metrics为模型评估标准.
-model.compile(loss='categorical_crossentropy',
+# adam = optimizers.adam_v2.Adam(lr=1e-05)  # Keras更新了，调用方式变了
+model.compile(loss='binary_crossentropy',
               optimizer='adam',
               metrics=['accuracy'])
 plot_model(model, to_file=result_dir + '/model.png')
@@ -75,10 +93,11 @@ result = model.fit(
     steps_per_epoch=train_iteration_count,
     epochs=epochs,
     validation_data=validation_generator,
-    validation_steps=val_iteration_count)
+    validation_steps=val_iteration_count,
+    callbacks=[reduce_lr])
 
 # 测试模型、评估分数
-score = model.evaluate(test_generator, steps=2)
+score = model.evaluate(test_generator, steps=5)
 print('测试分数：' + str(score))
 
 # 绘图并将模型json/weight信息导出
@@ -87,7 +106,7 @@ plt.plot(result.history['val_accuracy'])
 plt.title('model accuracy')
 plt.ylabel('accuracy')
 plt.xlabel('epochs')
-plt.legend(['train', 'test'], loc='upper left')
+plt.legend(['train', 'val'], loc='upper left')
 plt.show()
 
 plt.plot(result.history['loss'])
@@ -95,10 +114,10 @@ plt.plot(result.history['val_loss'])
 plt.title('model loss')
 plt.ylabel('loss')
 plt.xlabel('epochs')
-plt.legend(['train', 'test'], loc='upper left')
+plt.legend(['train', 'val'], loc='upper left')
 plt.show()
 
 json_string = model.to_json()
 open(result_dir + '/model_architecture.json', 'w').write(json_string)
 
-model.save_weights(result_dir + '/model_weights.h5')
+# model.save_weights(result_dir + '/model_weights.h5')

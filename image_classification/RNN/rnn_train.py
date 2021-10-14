05984592ba93f289b846e544e47e3751b1097b65
Ruyangchen <3226717170@qq.com>
2021-10-14 20:55:43 +0800

    rnn 使用lstm

--- image_classification/RNN/rnn_train.py
+++ image_classification/RNN/rnn_train.py
@@ -10,10 +10,8 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 # 图像参数
 img_width = 128
 img_height = 128
-# means_3channels = [0.4853868, 0.45147458, 0.4142927]
-# std_3channels = [0.22896309, 0.22360295, 0.22433776]
-means_3channels = [0.4853868]
-std_3channels = [0.22896309]
+means_3channels = [0.4853868, 0.45147458, 0.4142927]
+std_3channels = [0.22896309, 0.22360295, 0.22433776]
 
 # 超参数
 per_batch_size = 25  # 批量载入图片的个数
@@ -32,14 +30,12 @@ test_data_path = '../data/test'
 # ToTensor已经将图像数据归一化到[0, 1]
 # Normalize将图像数据进一步调整到[-1, 1]
 train_transform = transforms.Compose([
-    transforms.Grayscale(num_output_channels=1),
     transforms.RandomResizedCrop([img_width, ]),
     transforms.RandomHorizontalFlip(),
     transforms.ToTensor(),
     transforms.Normalize(means_3channels, std_3channels)
 ])
 validation_transform = transforms.Compose([
-    transforms.Grayscale(num_output_channels=1),
     transforms.ToTensor(),
     transforms.Normalize(means_3channels, std_3channels)
 ])
@@ -63,20 +59,23 @@ class ImageRnn(nn.Module):
         self.out_dim = out_dim  # 输出维度（也就是分类个数）
         self.input_dim = input_dim  # 输入维度
 
-        self.basic_rnn = nn.RNN(self.input_dim, self.neurons, num_layers=self.layers)
+        self.basic_rnn = nn.LSTM(self.input_dim*3, self.neurons, num_layers=self.layers)
         self.FC = nn.Linear(self.neurons, self.out_dim)
-
         self.hidden = self.init_hidden()
 
     def init_hidden(self):
         return torch.zeros(self.layers, self.batch_size, self.neurons).to(device)
 
-    def forward(self, x):
-        self.batch_size = x.size(0)
-        x = x.permute(1, 0, 2)  # 第一维度与第二维度换位
+    # 是对RNN的一层封装，自定义组合RNN。
+    def forward(self, data):
+        self.batch_size = data.size(0)
+        data = data.permute(1, 0, 2)  # 第一维度与第二维度换位
 
-        rnn_out, self.hidden = self.basic_rnn(x, self.hidden)  # 前向传播
-        out = self.FC(rnn_out[-1])  # 取RNN的最后一层，然后求出每一类概率. out为25*2
+        # 前向传播，rnn_out是128个序列的输出值。
+        rnn_out, self.hidden = self.basic_rnn.forward(data)
+        # print(rnn_out)
+        # 取RNN的最后一个序列，然后求出每一类概率.rnn_out的size为[75, 150] out为75*2
+        out = self.FC(rnn_out[-1])
         return out.view(-1, self.out_dim)
 
 
@@ -84,16 +83,28 @@ model = ImageRnn(per_batch_size, InputDim, OutDim, Neurons, Layers).to(device)
 criterion = nn.CrossEntropyLoss()
 optimizer = opt.Adam(model.parameters(), lr=0.001)
 
-# 初始化模型的weight
-model.basic_rnn.weight_hh_l0.data = torch.eye(n=Neurons, m=Neurons, out=None).to(device)
-model.basic_rnn.weight_hh_l1.data = torch.eye(n=Neurons, m=Neurons, out=None).to(device)
-model.basic_rnn.weight_hh_l2.data = torch.eye(n=Neurons, m=Neurons, out=None).to(device)
+# 初始化模型的weight, eye返回一个二维张量，对角线为1，其他地方为0。
+# model.basic_rnn.weight_hh_l0.data = torch.eye(n=Neurons, m=Neurons).to(device)
+# model.basic_rnn.weight_hh_l1.data = torch.eye(n=Neurons, m=Neurons).to(device)
+# model.basic_rnn.weight_hh_l2.data = torch.eye(n=Neurons, m=Neurons).to(device)
 
 
 def get_accuracy(log_it, target, batch_size):
     corrects = (torch.max(log_it, 1)[1].view(target.size()).data == target.data).sum()
     accuracy = 100.0 * corrects / batch_size
     return accuracy.item()
+
+
+def validation_accuracy():
+    test_acc = 0.0
+    for index1, (inputs, labels1) in enumerate(validation_loader):
+        labels1 = labels1.to(device)
+        inputs = inputs.view(-1, 128, 128).to(device)
+        outputs1 = model(inputs)
+        batch_acc = get_accuracy(outputs1, labels1, per_batch_size)
+        print("Batch:{:0>2d}, Accuracy : {:<6.4f}%".format(index1, batch_acc))
+        test_acc = test_acc + batch_acc
+    return test_acc / index1
 
 
 # 循环epoch个轮次
@@ -103,17 +114,12 @@ for epoch in range(epochs):
     model.train()  # 网络进入训练模式
 
     # enumerate内置函数，同时遍历索引和元素
-    for index, item in enumerate(train_loader):
+    for index, (input_data, labels) in enumerate(train_loader):
         optimizer.zero_grad()
-        # reset hidden states
         model.hidden = model.init_hidden()
 
-        # get inputs
-        input_data, labels = item  # 元素带着数据信息和标签信息 个数为batch_size
-        # input_data.size()为[25, 3, 128, 128]
-        input_data = input_data.view(-1, 128, 128).to(device)
+        input_data = input_data.view(input_data.size(0), 128, -1).to(device)
         labels = labels.to(device)
-        # forward+backward+optimize
         outputs = model.forward(input_data)
         loss = criterion(outputs, labels)
         loss.backward()
@@ -124,14 +130,4 @@ for epoch in range(epochs):
     msg = 'Epoch : {:0>2d} | Loss : {:<6.4f} | Train Accuracy : {:<6.2f}%'
     print(msg.format(epoch, train_running_loss / index, train_acc / index))
 
-test_acc = 0.0
-for i, data in enumerate(validation_loader, 0):
-    inputs, labels = data
-    labels = labels.to(device)
-    inputs = inputs.view(-1, 128, 128).to(device)
-    outputs = model(inputs)
-    thisBatchAcc = get_accuracy(outputs, labels, per_batch_size)
-    print("Batch:{:0>2d}, Accuracy : {:<6.4f}%".format(i, thisBatchAcc))
-    test_acc = test_acc + thisBatchAcc
-print('============平均准确率===========')
-print('Test Accuracy : {:<6.4f}%'.format(test_acc / i))
+print('Validation Accuracy : {:<6.4f}%'.format(validation_accuracy()))
